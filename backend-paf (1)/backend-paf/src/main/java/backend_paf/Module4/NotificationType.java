77cf0dc40dedd171671138fd41f6dbcdfe533f01
Nimasha128855 <nimashajayasingha431@gmail.com>
2026-04-19 18:11:09 +0530

    create notification entity, dto, repository, and enum

---
+++ backend-paf (1)/backend-paf/src/main/java/backend_paf/Module4/NotificationType.java
@@ -0,0 +1,12 @@
+package backend_paf.Module4;
+
+/**
+ * Enum representing the type of notification.
+ * Extend this enum as new module events are added.
+ */
+public enum NotificationType {
+    BOOKING_APPROVED,
+    BOOKING_REJECTED,
+    TICKET_STATUS_CHANGED,
+    NEW_TICKET_COMMENT
+}
