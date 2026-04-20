66fc49f591efdcb03a9274a10aacd9b2fb6607aa
Jananga07 <janangachandima07@gmail.com>
2026-04-21 00:05:41 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
 import Home from './Home/Home';
+import Navbar from './M2/components/Navbar'
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path='/nav' element={<Navbar/>}/>
     </Routes>
   );
 }
