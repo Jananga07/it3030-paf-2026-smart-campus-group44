918cf479b32d06300852ba39ed4dfe425f74ee6c
Sujan Sandeepa <s.sandeepatech@gmail.com>
2026-04-23 03:20:52 +0530

    Create ResourceRepository.java

---
+++ backend-paf (1)/backend-paf/src/main/java/backend_paf/Module1/repository/ResourceRepository.java
@@ -0,0 +1,9 @@
+package backend_paf.Module1.repository;
+
+import backend_paf.Module1.model.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
+}
