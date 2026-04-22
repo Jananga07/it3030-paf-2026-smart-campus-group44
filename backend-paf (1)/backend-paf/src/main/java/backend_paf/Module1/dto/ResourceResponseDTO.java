04a152bc5437e8eaed02f4dcf6c43ef9644791a8
Sujan Sandeepa <s.sandeepatech@gmail.com>
2026-04-23 03:21:05 +0530

    Create ResourceResponseDTO.java

---
+++ backend-paf (1)/backend-paf/src/main/java/backend_paf/Module1/dto/ResourceResponseDTO.java
@@ -0,0 +1,48 @@
+package backend_paf.Module1.dto;
+
+public class ResourceResponseDTO {
+
+    private Long id;
+    private String name;
+    private String type;
+    private String location;
+    private Integer capacity;
+    private String description;
+    private String status;
+    private String availabilityWindow;
+
+    public ResourceResponseDTO() {}
+
+    public ResourceResponseDTO(Long id, String name, String type, String location,
+                                Integer capacity, String description,
+                                String status, String availabilityWindow) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.location = location;
+        this.capacity = capacity;
+        this.description = description;
+        this.status = status;
+        this.availabilityWindow = availabilityWindow;
+    }
+
+    // GETTERS
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public String getType() { return type; }
+    public String getLocation() { return location; }
+    public Integer getCapacity() { return capacity; }
+    public String getDescription() { return description; }
+    public String getStatus() { return status; }
+    public String getAvailabilityWindow() { return availabilityWindow; }
+
+    // SETTERS
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setType(String type) { this.type = type; }
+    public void setLocation(String location) { this.location = location; }
+    public void setCapacity(Integer capacity) { this.capacity = capacity; }
+    public void setDescription(String description) { this.description = description; }
+    public void setStatus(String status) { this.status = status; }
+    public void setAvailabilityWindow(String availabilityWindow) { this.availabilityWindow = availabilityWindow; }
+}
