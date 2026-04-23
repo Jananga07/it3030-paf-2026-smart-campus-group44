b4c1da22bd97a7b2c521518bc623a6fffca2377f
kavindanimsara2050 <kavindanimsara2050@gmail.com>
2026-04-23 19:09:09 +0530

    Add backend  Models core files for Module 3
    build the model files

---
+++ backend-paf (1)/backend-paf/src/main/java/backend_paf/Module3/model/Role.java
@@ -0,0 +1,5 @@
+package backend_paf.Module3.model;
+
+public enum Role {
+    USER, ADMIN, TECHNICIAN
+}
---
+++ backend-paf (1)/backend-paf/src/main/java/backend_paf/Module3/model/User.java
@@ -0,0 +1,52 @@
+package backend_paf.Module3.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users_m3")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
