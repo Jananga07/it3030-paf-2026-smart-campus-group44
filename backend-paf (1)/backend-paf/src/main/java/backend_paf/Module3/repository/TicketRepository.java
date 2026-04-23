78271b566324fdf0a1cad66ee52fff68dca4d336
kavindanimsara2050 <kavindanimsara2050@gmail.com>
2026-04-23 19:56:30 +0530

    Add backend core  Repositary files for Module 3
    Add repositary files

---
+++ backend-paf (1)/backend-paf/src/main/java/backend_paf/Module3/repository/TicketRepository.java
@@ -0,0 +1,12 @@
+package backend_paf.Module3.repository;
+
+import backend_paf.Module3.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findByUserId(Long userId);
+}
