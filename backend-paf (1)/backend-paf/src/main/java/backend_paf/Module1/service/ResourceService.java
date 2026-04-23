99c0ce981beb337434e595e1fd7c41cacb3163eb
Sujan Sandeepa <s.sandeepatech@gmail.com>
2026-04-23 11:23:17 +0530

    Update ResourceService.java

--- backend-paf (1)/backend-paf/src/main/java/backend_paf/Module1/service/ResourceService.java
+++ backend-paf (1)/backend-paf/src/main/java/backend_paf/Module1/service/ResourceService.java
@@ -2,7 +2,9 @@ package backend_paf.Module1.service;
 
 import backend_paf.Module1.dto.ResourceRequestDTO;
 import backend_paf.Module1.dto.ResourceResponseDTO;
+import backend_paf.Module1.enums.ResourceStatus;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ResourceService {
@@ -16,5 +18,13 @@ public interface ResourceService {
     ResourceResponseDTO updateResource(Long id, ResourceRequestDTO requestDTO);
 
     void deleteResource(Long id);
+
+    List<ResourceResponseDTO> searchResources(String type, String location, Integer capacity, ResourceStatus status);
+
+    ResourceResponseDTO updateStatus(Long id, ResourceStatus status);
+
+    List<ResourceResponseDTO> getResourcesByStatus(ResourceStatus status);
+
+    List<ResourceResponseDTO> getAvailableResources(LocalDate from, LocalDate to);
 }
 
