4235d8041ae36873f336d1cdf09bc534000e4834
kavindanimsara2050 <kavindanimsara2050@gmail.com>
2026-04-23 21:43:15 +0530

    Add backend  File API files for Module 3
    ADD backenf Controller And Service files

---
+++ backend-paf (1)/backend-paf/src/main/java/backend_paf/Module3/service/FileStorageService.java
@@ -0,0 +1,60 @@
+package backend_paf.Module3.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    public FileStorageService(@Value("${file.upload-dir:uploads/}") String uploadDir) {
+        this.fileStorageLocation = Paths.get(uploadDir).toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        // Normalize file name
+        String originalFileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            // Check if the file's name contains invalid characters
+            if (originalFileName.contains("..")) {
+                throw new RuntimeException("Sorry! Filename contains invalid path sequence " + originalFileName);
+            }
+
+            // Generate unique file name
+            String fileExtension = "";
+            int i = originalFileName.lastIndexOf('.');
+            if (i > 0) {
+                fileExtension = originalFileName.substring(i);
+            }
+            // Allow only jpg, jpeg, png
+            if(!fileExtension.equalsIgnoreCase(".jpg") && 
+               !fileExtension.equalsIgnoreCase(".jpeg") && 
+               !fileExtension.equalsIgnoreCase(".png")) {
+                throw new RuntimeException("Sorry! File type not allowed: " + fileExtension);
+            }
+
+            String newFileName = UUID.randomUUID().toString() + fileExtension;
+            Path targetLocation = this.fileStorageLocation.resolve(newFileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return newFileName;
+        } catch (IOException ex) {
+            throw new RuntimeException("Could not store file " + originalFileName + ". Please try again!", ex);
+        }
+    }
+}
